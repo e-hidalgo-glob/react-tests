0b0ca6acd247be2082dd96947ae744c3644f5ab7
Emiliano Hidalgo Castaneda <emiliano.hidalgo@globant.com>
2022-12-29 21:32:54 -0600

    01-sol

--- 01-birthday-reminder/setup/src/App.js
+++ 01-birthday-reminder/setup/src/App.js
@@ -1,8 +1,21 @@
-import React, { useState } from 'react';
-import data from './data';
-import List from './List';
+import React, { useState } from "react";
+import data from "./data";
+import List from "./List";
+
 function App() {
-  return <h2>reminder project setup</h2>;
+  const [peopleBirthdays, setPeopleBirthdays] = useState(data);
+
+  const handleClearClick = () => setPeopleBirthdays([]);
+
+  return (
+    <main>
+      <section className="container">
+        <h3>Hoy hay {peopleBirthdays.length} cumpleaños</h3>
+        <List peopleBirthdays={peopleBirthdays}/>
+        <button onClick={handleClearClick}>Limpiar lista</button>
+      </section>
+    </main>
+  );
 }
 
 export default App;
--- 01-birthday-reminder/setup/src/List.js
+++ 01-birthday-reminder/setup/src/List.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React from "react";
 
-const List = () => {
+const List = ({ peopleBirthdays }) => {
+
+  const generateListItem = (birthday) => {
+    const { id, name, age, image } = birthday;
+    return (
+      <article key={id} className="person">
+        <img src={image} alt={name} />
+        <div>
+          <h4>{name}</h4>
+          <p>{age} years</p>
+        </div>
+      </article>
+    );
+  };
+
+  const items = peopleBirthdays.map((bday) => generateListItem(bday));
   return (
     <>
-      <h2>list component</h2>
+      {items}
     </>
   );
 };
