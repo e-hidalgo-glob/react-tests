7b37c529001c15cf91244ba75ebfb3de5ccedcc6
Emiliano Hidalgo Castaneda <emiliano.hidalgo@globant.com>
2022-12-30 10:46:52 -0600

    08-sol

--- 08-lorem-ipsum/setup/src/App.js
+++ 08-lorem-ipsum/setup/src/App.js
@@ -1,11 +1,36 @@
-import React, { useState } from 'react';
-import data from './data';
+import React, { useState } from "react";
+import data from "./data";
 function App() {
-  
-  
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState([]);
+  const maxAmount = 8;
+  const minAmount = 1;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const value = Math.max(minAmount, Math.min(maxAmount, Number(count)));
+    setText(data.slice(0, value));
+  };
   return (
-  <h2>lorem ipsum project setup</h2>
-    )
+    <section className="section-center">
+      <h3>tired of boring lorem ipsum?</h3>
+      <form className="lorem-form" onSubmit={handleSubmit}>
+        <label htmlFor="amount">paragraphs:</label>
+        <input
+          type="number"
+          name="amount"
+          id="amount"
+          value={count}
+          onChange={(e) => setCount(e.target.value)}
+        />
+        <button className="btn">generate</button>
+      </form>
+      <article className="lorem-text">
+        {text.map((item, index) => (
+          <p key={index}>{item}</p>
+        ))}
+      </article>
+    </section>
+  );
 }
 
 export default App;
