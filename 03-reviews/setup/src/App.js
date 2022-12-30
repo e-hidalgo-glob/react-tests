aa6d3e6eaeef82f963d814e134f9a71f1c6a9dc4
Emiliano Hidalgo Castaneda <emiliano.hidalgo@globant.com>
2022-12-29 22:54:57 -0600

    03-sol

--- 03-reviews/setup/src/App.js
+++ 03-reviews/setup/src/App.js
@@ -1,7 +1,15 @@
-import React from 'react';
-import Review from './Review';
-function App() {
-  return <h2>reviews project setup</h2>;
-}
+import React from "react";
+import Review from "./Review";
+const App = () => (
+  <main>
+    <section className="container">
+      <div className="title">
+        <h2>Reseñas</h2>
+        <div className="underline"></div>
+      </div>
+      <Review />
+    </section>
+  </main>
+);
 
 export default App;
